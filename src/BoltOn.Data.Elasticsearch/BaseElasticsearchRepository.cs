12d30811eb03d8cf7e66b4c4d70fca14eb8e24e8
arjohnson1 <arjohnson1@pm.me>
2019-10-22 05:34:00 -0400

    Elasticsearch setup

---
+++ src/BoltOn.Data.Elasticsearch/BaseElasticsearchRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Nest;
+
+namespace BoltOn.Data.Elasticsearch
+{
+    public abstract class BaseElasticsearchRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        private readonly IElasticClient _elasticClient;
+
+        protected BaseElasticsearchRepository(IElasticClient elasticClient)
+        {
+            _elasticClient = elasticClient;
+        }
+
+        public TEntity Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> AddAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> FindBy(Expression<Func<TEntity, bool>> predicate, params Expression<Func<TEntity, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> FindByAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken = default, params Expression<Func<TEntity, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(object id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/BoltOn.Data.Elasticsearch/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using BoltOn.Bootstrapping;
+
+namespace BoltOn.Data.Elasticsearch
+{
+    public static class Extensions
+    {
+        public static BoltOnOptions BoltOnElasticsearchModule(this BoltOnOptions boltOnOptions)
+        {
+            boltOnOptions.BoltOnAssemblies(Assembly.GetExecutingAssembly());
+            return boltOnOptions;
+        }
+    }
+}
